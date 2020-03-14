a8faee7459cd86331175a5b4f80b602ed5a9d3eb
qdenisq <qdenisq@gmail.com>
2020-03-14 14:16:19 +1100

    DS: implement encoder

--- synthesizer/models/tacotron_2.py
+++ synthesizer/models/tacotron_2.py
@@ -1,4 +1,7 @@
+
 import torch
+from torch import nn
+from torch.nn import functional as F
 from synthesizer.utils.symbols import symbols
 from synthesizer.infolog import log
 from synthesizer.models.helpers import TacoTrainingHelper, TacoTestHelper
@@ -22,9 +25,79 @@ def split_func(x, split_pos):
     return rst
 
 
+class ConvNorm(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,
+                 padding=None, dilation=1, bias=True, w_init_gain='linear'):
+        super(ConvNorm, self).__init__()
+        if padding is None:
+            assert(kernel_size % 2 == 1)
+            padding = int(dilation * (kernel_size - 1) / 2)
+
+        self.conv = torch.nn.Conv1d(in_channels, out_channels,
+                                    kernel_size=kernel_size, stride=stride,
+                                    padding=padding, dilation=dilation,
+                                    bias=bias)
+
+        torch.nn.init.xavier_uniform_(
+            self.conv.weight, gain=torch.nn.init.calculate_gain(w_init_gain))
+
+    def forward(self, signal):
+        conv_signal = self.conv(signal)
+        return conv_signal
+
+
 class Encoder(torch.nn.Module):
     def __init__(self, hparams):
         super(Encoder, self).__init__()
+        self._hparams = hparams
+        dims = [hparams.character_embedding_dim] + hparams.conv_dim
+
+        self.conv_layers = nn.ModuleList()
+        for i in range(len(dims) - 2):
+            self.conv_layers.append(nn.Sequential(
+                    ConvNorm(dims[i],
+                             dims[i+1],
+                             kernel_size=hparams.kernel_size,
+                             stride=1,
+                             padding=int((hparams.kernel_size - 1) / 2),
+                             dilation=1,
+                             w_init_gain='relu'),
+                    nn.BatchNorm1d(hparams.encoder_embedding_dim)
+            ))
+
+        lstm_dim = hparams.lstm_dim if not hparams.lstm_bidirectional else hparams.lstm_dim // 2
+        self.lstm = nn.LSTM(dims[-1], lstm_dim, 1, batch_first=True, bidirectional=hparams.lstm_bidirectional)
+
+    def forward(self, x, input_lengths):
+        for c in self.conv_layers:
+            x = F.dropout(F.relu(c(x)), self._hparams.dropout, self.training)
+
+        x = x.transpose(1, 2)
+
+        # pytorch tensor are not reversible, hence the conversion
+        input_lengths = input_lengths.cpu().numpy()
+        x = nn.utils.rnn.pack_padded_sequence(
+            x, input_lengths, batch_first=True)
+
+        self.lstm.flatten_parameters()
+        outputs, _ = self.lstm(x)
+
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(
+            outputs, batch_first=True)
+
+        return outputs
+
+    def inference(self, x):
+        for c in self.conv_layers:
+            x = F.dropout(F.relu(c(x)), self._hparams.dropout, self.training)
+
+        x = x.transpose(1, 2)
+
+        self.lstm.flatten_parameters()
+        outputs, _ = self.lstm(x)
+
+        return outputs
+
 
 
 class Decoder(torch.nn.Module):
@@ -49,6 +122,9 @@ class Attention(torch.nn.Module):
 
 
 
+
+
+
 class Tacotron2(torch.nn.Module):
     """
     Tacotron-2 PyTorch
@@ -65,8 +141,18 @@ class Tacotron2(torch.nn.Module):
         self.encoder = Encoder(hparams)
         self.decoder = Decoder(hparams)
 
-    def forward(self, *input: Any, **kwargs: Any) -> T_co:
+    def forward(self, inputs):
+        text_inputs, text_lengths, mels, max_len, output_lengths = inputs
+        text_lengths, output_lengths = text_lengths.data, output_lengths.data
 
+        embedded_inputs = self.character_embedding(text_inputs).transpose(1,2)
+        encoder_outputs = self.encoder(embedded_inputs, text_lengths)
+
+        mel_outputs, gate_outputs, alignments = self.decoder(
+            encoder_outputs, mels, memory_lengths=text_lengths)
+
+        mel_outputs_postnet = self.postnet(mel_outputs)
+        mel_outputs_postnet = mel_outputs + mel_outputs_postnet
 
         #
         # log("Initialized Tacotron model. Dimensions (? = dynamic shape): ")
